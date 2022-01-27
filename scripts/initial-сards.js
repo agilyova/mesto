85917b6b91669c9a36ad1a263444566ab84bc8b1
Aleksandra Gilyova <gilyova.a.v@gmail.com>
2022-01-27 21:41:41 +0300

    fix: after review

---
+++ scripts/initial-сards.js
@@ -0,0 +1,26 @@
+const initialCards = [
+  {
+    name: 'Калининград',
+    link: 'images/kaliningrad.jpg'
+  },
+  {
+    name: 'Казань',
+    link: 'images/Kazan.jpg'
+  },
+  {
+    name: 'Екатеринбург',
+    link: 'images/Ekaterinburg.jpg'
+  },
+  {
+    name: 'Санкт-Петербург',
+    link: 'images/St.Petersburg.jpg'
+  },
+  {
+    name: 'Красная поляна',
+    link: 'images/Krasnaya_polyana.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'images/Baikal.jpg'
+  }
+];
